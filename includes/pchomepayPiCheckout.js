eb53cd892059fbdca1e16a1cd33baa0379c0108a
will.wang <will.wang@piapp.com.tw>
2024-04-16 18:13:54 +0800

    add block

--- includes/pchomepayPiCheckout.js
+++ includes/pchomepayPiCheckout.js
@@ -1,17 +1,17 @@
 const settingsPi = window.wc.wcSettings.getSetting( 'pchomepay_pi_data', {} );
 const labelPi = window.wp.htmlEntities.decodeEntities( settingsPi.title ) || window.wp.i18n.__( 'Pchomepay Pi', 'pchomepay_pi' );
-const Content = () => {
+const ContentPi = () => {
     return window.wp.htmlEntities.decodeEntities( settingsPi.description || '' );
 };
-const Block_Gateway = {
+const Block_Gateway_Pi = {
     name: 'pchomepay_pi',
     label: labelPi,
-    content: Object( window.wp.element.createElement )( Content, null ),
-    edit: Object( window.wp.element.createElement )( Content, null ),
+    content: Object( window.wp.element.createElement )( ContentPi, null ),
+    edit: Object( window.wp.element.createElement )( ContentPi, null ),
     canMakePayment: () => true,
     ariaLabel: labelPi,
     supports: {
         features: settingsPi.supports,
     },
 };
-window.wc.wcBlocksRegistry.registerPaymentMethod( Block_Gateway );
+window.wc.wcBlocksRegistry.registerPaymentMethod( Block_Gateway_Pi );
