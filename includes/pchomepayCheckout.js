1b0446ae6cc7f8543fdda027c8e202a0700e62d8
will.wang <will.wang@piapp.com.tw>
2024-04-16 18:56:38 +0800

    add block

--- includes/pchomepayCheckout.js
+++ includes/pchomepayCheckout.js
@@ -3,15 +3,11 @@ const labelPchomepay = window.wp.htmlEntities.decodeEntities( settingsPchomepay.
 const ContentPchomepay = () => {
     return window.wp.htmlEntities.decodeEntities( settingsPchomepay.description || '' );
 };
-const iconUrlPchomepay = '../images/pchomepay_logo.png';
 const BlockGatewayPchomepay = {
     name: 'pchomepay',
     label: labelPchomepay,
     content: Object( window.wp.element.createElement )( ContentPchomepay, null ),
     edit: Object( window.wp.element.createElement )( ContentPchomepay, null ),
-    icons: {
-        src: iconUrlPchomepay,
-    },
     canMakePayment: () => true,
     ariaLabel: labelPchomepay,
     supports: {
