42232ca81b155b6ffa819818a4be2dc1200f309b
will.wang <will.wang@piapp.com.tw>
2024-04-16 16:44:21 +0800

    add block

--- includes/class-block.php
+++ includes/class-block.php
@@ -37,58 +37,12 @@ class Pchomepay_Gateway_Blocks extends AbstractPaymentMethodType {
         }
         return [ 'pchomepay-blocks-integration' ];
     }
-
     public function get_payment_method_data() {
         return [
             'title' => $this->gateway->title,
             'description' => $this->gateway->description,
         ];
     }
-
 }
 
-class Pchomepay_Pi_Gateway_Blocks extends AbstractPaymentMethodType {
-
-    private $gateway;
-    protected $name = 'pchomepay_pi';// your payment gateway name
-
-    public function initialize() {
-        $this->settings = get_option( 'woocommerce_my_custom_gateway_settings', [] );
-        $this->gateway = new WC_PI_Gateway_PChomePay();
-    }
-
-    public function is_active() {
-        return $this->gateway->is_available();
-    }
-
-    public function get_payment_method_script_handles() {
-
-        wp_register_script(
-            'pchomepay-blocks-integration',
-            plugin_dir_url(__FILE__) . 'checkout.js',
-            [
-                'wc-blocks-registry',
-                'wc-settings',
-                'wp-element',
-                'wp-html-entities',
-                'wp-i18n',
-            ],
-            null,
-            true
-        );
-        if( function_exists( 'wp_set_script_translations' ) ) {            
-            wp_set_script_translations( 'pchomepay-blocks-integration');
-            
-        }
-        return [ 'pchomepay-blocks-integration' ];
-    }
-
-    public function get_payment_method_data() {
-        return [
-            'title' => $this->gateway->title,
-            'description' => $this->gateway->description,
-        ];
-    }
-
-}
 ?>
